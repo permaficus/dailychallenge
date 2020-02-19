02e1057511d857d0eff1240981269b0371a97c0b
permaficus <abukhalif2019@gmail.com>
2020-02-19 22:28:07 +0700

    Entek duren 3 biji, Nescafe 1 cangkir, tempe penyet 2

---
+++ make.js
@@ -0,0 +1,14 @@
+make = (n,char_) => {
+
+    char_ = char_ || ''
+    let dict = '.abcdefghijklmnopqrstuvqxyz', char ='';
+    char += `${dict[n]}`;
+    n--;
+    char_ += `${char}`;
+    return (n == 0) ? `${char}-${char_.substr(0,char_.length-1).split('').reverse().join('-')}`
+            :`${char}-${make(n,char_)}`
+
+}
+
+console.log(make(5))
+console.log(make(10))
