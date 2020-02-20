ee3cd91670de47635911f74cc6a48e605f54f4a2
permaficus <abukhalif2019@gmail.com>
2020-02-20 15:32:02 +0700

    Change something

--- make.js
+++ make.js
@@ -1,17 +1,33 @@
-make = (n,char_) => {
+// make = (n, immutableN, desc = true) => {
+make = (n, char_) => {
+    let dict = '.abcdefghijklmnopqrstuvqxyz', char ='';
 
     // Credit to @padulkemid
 
     char_ = char_ || ''
-    let dict = '.abcdefghijklmnopqrstuvqxyz', char ='';
     char += dict[n];
     char_ += char;
     n--;
     return (n == 0) ? `${char}-${char_.substr(0,char_.length-1).split('').reverse().join('-')}`
             :`${char}-${make(n,char_)}`
 
+    // Credit to @CoachSemmi
+    
+    if (!immutableN) {
+        immutableN = n
+    } else if (n === immutableN) {
+        return ''
+    }
+
+    if (n === 1 || !desc) {
+        return dict[n] + make(n + 1, immutableN, false)
+    } else {
+        return dict[n] + make(n-1, immutableN)
+    }
+
 }
 
 console.log(make(5))
+return
 console.log(make(10))
 console.log(make(26))
